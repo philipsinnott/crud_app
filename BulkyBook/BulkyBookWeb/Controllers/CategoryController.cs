006f785a56a0982d885950d84960c55f7a1ab96c
Philip Sinnott <philipsinnotts@gmail.com>
2022-06-13 23:17:49 +0200

    Added basic alerts for CRUD actions

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -73,6 +74,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category updated successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -109,6 +111,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
+                TempData["Success"] = "Category deleted successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
