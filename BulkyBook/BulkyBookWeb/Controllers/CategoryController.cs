45e6e12952371366d2989528f0591fb1378d77ed
Philip Sinnott <philipsinnotts@gmail.com>
2022-06-13 22:17:31 +0200

    Added create category functionality

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,24 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
